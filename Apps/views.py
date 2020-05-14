92f369afc9413cf44882774c416ec4ea2a48b885
BaoHaoo <36300930+BaoHaoo@users.noreply.github.com>
2020-05-14 11:48:36 +0800

    Update views.py
    简单的把登录注册分开并处理，还没有连接数据库

--- Apps/views.py
+++ Apps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
 def test(request):
@@ -17,14 +18,36 @@ def login(request):
 		print('Post:')
 		if checkuser(name,pwd) == True:
 			return HttpResponseRedirect('../main')
-		else:
+		else: #判断是否有注册信息
 			messages.success(request,"用户名或密码有误")
-
-	print(name,pwd)
+	#
+	# print(name,pwd)
 	return render(request,'login.html')
 
 #检查用户名和密码
 def checkuser(name,pwd):
+	if name == None or pwd == None:
+		return False
 	if name[:4] == '2017' and pwd != '':
 		return True
 	return False
+
+#得到用户注册的数据
+def register(request):
+	reg_name = request.POST.get ( 'reg_name' )
+	reg_pwd = request.POST.get ( 'reg_pwd' )
+	reg_inst = request.POST.get ( 'reg_inst' )
+	reg_major = request.POST.get ( 'reg_major' )
+	reg_grade = request.POST.get ( 'reg_grade' )
+	reg_sex = request.POST.get ( 'reg_sex' )
+
+	print(reg_name)
+	print(reg_pwd)
+	print(reg_inst)
+	print(reg_major)
+	print(reg_grade)
+	print(reg_sex)
+
+	messages.success(request,"注册成功")
+
+	return HttpResponseRedirect('../login')
